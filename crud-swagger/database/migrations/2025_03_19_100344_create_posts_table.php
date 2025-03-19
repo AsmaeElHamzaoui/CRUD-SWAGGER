f6c7fb780c6c767e2f6a8aa9171754f591e8ecfa
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-19 10:05:37 +0000

    ajout des colonnes pour la migration posts

--- crud-swagger/database/migrations/2025_03_19_100344_create_posts_table.php
+++ crud-swagger/database/migrations/2025_03_19_100344_create_posts_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
+    
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
